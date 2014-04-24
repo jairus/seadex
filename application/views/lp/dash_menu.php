487075716a0aa675c25a5de75a8977803fa3e3b3
Jairus Bondoc <jairus@nmgresources.ph>
2014-04-24 18:37:07 +0800

    changepass lp

---
+++ application/views/lp/dash_menu.php
@@ -0,0 +1,16 @@
+<table class="table table-striped">
+  <thead>
+	<tr>
+	  <th class="startend">Menu</th>
+	</tr>
+  </thead>
+  <tbody>
+	<tr>
+	  <td>
+		<div class="menu"><a href="<?php echo site_url("lp"); ?>/mybids">My Bids</a></div>
+		<div class="menu"><a href="<?php echo site_url("lp"); ?>/acceptedbuds">Accepted Bids</a></div>
+		<div class="menu"><a href="<?php echo site_url("lp"); ?>/changepass">Change Password</a></div>
+	  </td>
+	</tr>
+  </tbody>
+</table>
